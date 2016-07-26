034278d54d0a1948cb27366c4bb2a1563d94e440
Cynthia (Qingwei) Li <cynngah@gmail.com>
2016-07-25 20:35:04 -0400

    only works with 1 wiki element and cannot unexpand LOL
    will fix this

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,6 @@
+var link;
+var el;
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -12,9 +15,10 @@ chrome.extension.sendMessage({}, function(response) {
         AList.forEach(srcElement => {
               // check if element is an anchor tag.
               if (srcElement.nodeName == 'A') {
-                  var link = srcElement.getAttribute('href');
+                  link = srcElement.getAttribute('href');
                   // check if it's got an href and if it comes from wikipedia
-                  if (link && (link.match('.*wikipedia.*', 'i'))) {
+                  if (link && (link.match('.*wikipedia*', 'i'))) {
+                      el = srcElement;
                       // call mouseover crap
                       addExpansionButton(srcElement);
                   }
@@ -24,26 +28,55 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
-
 /**
 * Add expansion button beside wiki tags
 *
 * @param element dom element to be modified/add javascript/css behavior to
 */
 function addExpansionButton(element) {
-    console.log('BUTTON MADE');
     // make button
     var expandButton = document.createElement('button');
     expandButton.className = 'expandButton';
     // put button after link
+    expandButton.addEventListener('click', requestExpand, false);
     element.insertAdjacentElement('afterend', expandButton);
-    expandButton.addEventListener('click', expandElement, false);
 }
 
-/**
-* Modify the page
-*/
-function expandElement(element) {
-    console.log('HELLO WORLD');
-    // add the description/abstract part of wikipedia page in a little blurb
+function requestExpand(element) {
+    xhrRequest();
+    // STEPS!
+    // 1. form query
+    // 2. make request to wikipedia
+    // 3. get information in json
+    // 4. get abstract
+    // 5. make webview
+    // 6. put abstract into webview
+    // 7. append webview afterend to object
+}
+
+function xhrRequest() {
+    var xhr = new XMLHttpRequest();
+    var name = link.substring(link.lastIndexOf('/') + 1);
+    var wikiReq = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=';
+    wikiReq += name.replace('_', '%20').replace('(', '%28').replace(')', '%29');
+    xhr.open('GET', wikiReq, true);
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', expandData);
+
+    xhr.send();
+}
+
+function expandData(e) {
+    var content = e.currentTarget.response.query.pages;
+    var index = first(content);
+    var blurb = content[index].extract;
+    var paragraph = document.createElement('p');
+    paragraph.innerHTML = blurb;
+    el.insertAdjacentElement('afterend', paragraph);
+}
+
+function first(obj) {
+    for (var a in obj) {
+        return a;
+    }
 }
